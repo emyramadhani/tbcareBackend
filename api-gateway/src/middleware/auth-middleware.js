fd20cdfdef741ec31292566df9653829a03a64c2
emyramadhani <ramadhaniemy10@gmail.com>
2026-03-25 20:50:33 +0700

    update konfigurasi upload kontent edukasi

--- api-gateway/src/middleware/auth-middleware.js
+++ api-gateway/src/middleware/auth-middleware.js
@@ -1,30 +1,35 @@
 const jwt = require('jsonwebtoken');
 
+
 const authMiddleware = (req, res, next) => {
 
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+  const userIdFromGateway = req.headers['x-user-id'];
+  const userRoleFromGateway = req.headers['x-user-role'];
 
-    if (!token) {
-        return res.status(401).json({
-            success: false,
-            message: 'Akses ditolak. Token tidak ditemukan.',
-        });
-    }
+  if (userIdFromGateway) {
+    req.userId = userIdFromGateway;
+    req.userRole = userRoleFromGateway;
+    return next();
+  }
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
 
-        req.headers['x-user-id'] = decoded.id;
-        req.headers['x-user-email'] = decoded.email;
-        req.headers['x-user-role'] = decoded.role;
-        next();
-    } catch (err) {
-        return res.status(401).json({
-            success: false,
-            message: 'Token tidak valid atau sudah expired.',
-        });
-    }
+  if (!token) {
+    return errorResponse(res, 'Akses ditolak. Token tidak ditemukan.', 401);
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.id;
+    req.userRole = decoded.role; 
+    
+    req.headers['x-user-role'] = decoded.role; 
+    
+    next();
+  } catch (err) {
+    return errorResponse(res, 'Token tidak valid atau sudah expired.', 401);
+  }
 };
 
 module.exports = authMiddleware;
