753eb36399c036fce18c9a111572536806cc2ca4
emyramadhani <ramadhaniemy10@gmail.com>
2026-03-31 00:32:56 +0700

    update fungsi blacklist token

--- user-service/src/middleware/auth-middleware.js
+++ user-service/src/middleware/auth-middleware.js
@@ -1,15 +1,8 @@
 const jwt = require('jsonwebtoken');
-const { errorResponse } = require('../utils/response');
 const BlacklistedToken = require('../models/blacklisted-token');
+const { errorResponse } = require('../utils/response');
 
-const authMiddleware = async (req, res, next) => { 
-  const userIdFromGateway = req.headers['x-user-id'];
-
-  if (userIdFromGateway) {
-    req.userId = userIdFromGateway;
-    return next();
-  }
-
+const authMiddleware = async (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
 
@@ -18,18 +11,21 @@ const authMiddleware = async (req, res, next) => {
   }
 
   try {
-  
     const isBlacklisted = await BlacklistedToken.findOne({ token });
     if (isBlacklisted) {
-      return errorResponse(res, 'Sesi telah berakhir. Silakan login kembali.', 401);
+      return errorResponse(res, 'Sesi telah berakhir (Logout). Silakan login kembali.', 401);
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded.id;
-    req.token = token; 
+    req.userRole = decoded.role;
+    
     next();
   } catch (err) {
-    return errorResponse(res, 'Token tidak valid atau sudah expired.', 401);
+    if (err.name === 'TokenExpiredError') {
+      return errorResponse(res, 'Token sudah expired. Silakan login kembali.', 401);
+    }
+    return errorResponse(res, 'Token tidak valid.', 401);
   }
 };
 
