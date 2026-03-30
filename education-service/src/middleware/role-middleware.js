be55bca45f158eecd9de0b643528723771d60edd
emyramadhani <ramadhaniemy10@gmail.com>
2026-03-31 00:09:06 +0700

    update bug auth middlerware untuk masing masing service

--- education-service/src/middleware/role-middleware.js
+++ education-service/src/middleware/role-middleware.js
@@ -1,14 +1,14 @@
 const { errorResponse } = require('../utils/response');
 
 const isAdmin = (req, res, next) => {
-
-    const userRole = req.headers['x-user-role'] || req.userRole;
-
-    if (userRole !== 'admin') {
-        return errorResponse(res, 'Akses ditolak. Hanya Admin yang dapat melakukan aksi ini.', 403);
-    }
     
-    next();
+  const userRole = req.userRole || req.headers['x-user-role'];
+
+  if (userRole !== 'admin') {
+    return errorResponse(res, 'Akses ditolak. Hanya Admin yang dapat melakukan aksi ini.', 403);
+  }
+  
+  next();
 };
 
 module.exports = { isAdmin };
--- reminder-medicine-service/src/middleware/auth-middleware.js
+++ reminder-medicine-service/src/middleware/auth-middleware.js
@@ -1,28 +1,19 @@
-const jwt = require('jsonwebtoken');
 const { errorResponse } = require('../utils/response');
 
 const authMiddleware = (req, res, next) => {
-  const userIdFromGateway = req.headers['x-user-id'];
+  
+  const userId = req.headers['x-user-id'];
+  const userRole = req.headers['x-user-role'];
 
-  if (userIdFromGateway) {
-    req.userId = userIdFromGateway;
-    return next();
+  if (!userId || !userRole) {
+
+    return errorResponse(res, 'Akses ditolak. Permintaan harus melalui API Gateway yang terautentikasi.', 401);
   }
 
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-
-  if (!token) {
-    return errorResponse(res, 'Akses ditolak. Token tidak ditemukan.', 401);
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.id;
-    next();
-  } catch (err) {
-    return errorResponse(res, 'Token tidak valid atau sudah expired.', 401);
-  }
+  req.userId = userId;
+  req.userRole = userRole;
+  
+  next();
 };
 
 module.exports = authMiddleware;
